a3a22c2853d79bec26467827c932ea6f9587827f
Venkata Gandi <venkata.gandi@cognizant.com>
2020-05-02 15:29:12 +0100

    added angular router reference to the application

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,34 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { ProductData } from "./products/product-data";
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+import { AppComponent } from "./app.component";
+import { WelcomeComponent } from "./home/welcome.component";
+import { PageNotFoundComponent } from "./page-not-found.component";
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
-import { UserModule } from './user/user.module';
-import { MessageModule } from './messages/message.module';
+import { ProductModule } from "./products/product.module";
+import { UserModule } from "./user/user.module";
+import { MessageModule } from "./messages/message.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot([]),
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
     UserModule,
-    MessageModule
+    MessageModule,
   ],
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    PageNotFoundComponent
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
